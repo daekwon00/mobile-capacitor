c70887840379dc6a339a594773ad67536f3a35c2
daekwon yoo <yoosw00@gmail.com>
2026-03-12 23:29:21 +0900

    로그인 아이디 입력 시 자동 대문자 변환 방지
    - autoCapitalize="none", autoCorrect="off" 속성 추가
    - iOS 키보드에서 첫 글자가 대문자로 바뀌는 문제 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -51,6 +51,8 @@ export default function LoginPage() {
               onChange={(e) => setUsername(e.target.value)}
               required
               autoComplete="username"
+              autoCapitalize="none"
+              autoCorrect="off"
               className="w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
           </div>
